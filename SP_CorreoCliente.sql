bffda5b7a8c0b45172966cb030352750dcbdd201
eriraldo <ericksalazar_g@hotmail.com>
2019-06-02 23:34:49 -0600

    sp del correo agregado

---
+++ SP_CorreoCliente.sql
@@ -0,0 +1,9 @@
+﻿create procedure SP_CorreoCliente
+	@usuario varchar(20),
+	@password varchar(30)
+as
+begin
+	DECLARE @idCliente int
+	select a.correo from Cliente a where a.usuario = @usuario and @password = a.contrasena
+	
+end
